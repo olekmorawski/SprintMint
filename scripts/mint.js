2eacdab5c6444ae8efc71e40b5f2734fe39c9395
Aleksander <olek.morawski@gmail.com>
2023-09-03 07:21:49 +0200

    ech

--- scripts/mint.js
+++ scripts/mint.js
@@ -8,16 +8,27 @@ async function main() {
   const simpleNFT = SimpleNFT.attach(contractAddress);
 
   // Mint a new NFT
-  const uri = "ipfs://https://gateway.pinata.cloud/ipfs/QmSC8wGPQJCsG4EZLNasqJvBcAW78XSUK8JWeGrU8C5XGR";
-  const tx = await simpleNFT.connect(owner).mintNFT(owner.address, uri);
-  await tx.wait();
+  const uri = "ipfs://QmSC8wGPQJCsG4EZLNasqJvBcAW78XSUK8JWeGrU8C5XGR"; 
 
-  console.log(`NFT Minted! Check it out at: https://explorer.testnet.mantle.xyz/tx/${tx.hash}`);
+  try {
+    // Estimate gas
+    const gasEstimate = await simpleNFT.connect(owner).estimateGas.mintNFT(owner.address, uri);
+
+    // Send transaction
+    const tx = await simpleNFT.connect(owner).mintNFT(owner.address, uri, {
+      gasLimit: gasEstimate,
+    });
+    await tx.wait();
+
+    console.log(`NFT Minted! Check it out at: https://explorer.testnet.mantle.xyz/tx/${tx.hash}`);
+  } catch (error) {
+    console.error('Minting failed:', error);
+  }
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-      console.error(error);
-      process.exit(1);
+    console.error(error);
+    process.exit(1);
   });
